41763f5c93c4c705dfd43abcabf7a6e4161d3fe5
Wesselkoole <104504409+Wesselkoole@users.noreply.github.com>
2025-02-28 13:59:06 +0100

    Delete js directory

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	fetch("content/header.html") 
-		.then(response => response.text())
-		.then(data => {
-			document.getElementById("header").innerHTML = data;
-		})
-		.catch(error => console.error("Error loading header:", error));
-});
