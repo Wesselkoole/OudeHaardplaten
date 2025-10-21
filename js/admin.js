a4d55837abbeecd6025112ddcd97ac8541a4addf
Wesselkoole <104504409+Wesselkoole@users.noreply.github.com>
2025-10-21 16:32:13 +0200

    Update admin.js
    Signed-off-by: Wesselkoole <104504409+Wesselkoole@users.noreply.github.com>

--- js/admin.js
+++ js/admin.js
@@ -3,20 +3,11 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 const SUPABASE_URL = "https://zwczthjsxzstedtodgfe.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Y3p0aGpzeHpzdGVkdG9kZ2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzA1NDEsImV4cCI6MjA3NjIwNjU0MX0.R1MFe-WIb1Nju-D27BfY1BiFkDo_V_PKthMP3hhkhDc";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-const supabase_service = createClient(SUPABASE_URL, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Y3p0aGpzeHpzdGVkdG9kZ2ZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDYzMDU0MSwiZXhwIjoyMDc2MjA2NTQxfQ.1Q3UfgSh1U8dWC4C0ofB5KBpZ9wGfGVZ6ln8HuAxvWg");
 
 // DOM elementen
 const loginContainer = document.getElementById("login-container");
 const adminFormContainer = document.getElementById("admin-form-container");
 
-// const { data, error } = await supabase_service.auth.admin.createUser({
-//   email: 'oudehaardplaten@gmail.com',
-//   password: '@Oudeplaten562!',
-//   email_confirm: true
-// })
-
-// if (error) console.log(error)
-// else console.log(data)
 document.getElementById("login-btn").addEventListener("click", async () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -80,4 +71,5 @@ document.getElementById("submit-btn").addEventListener("click", async (e) => {
     document.getElementById("admin-form-container").reset;
   }
 });
+
 
